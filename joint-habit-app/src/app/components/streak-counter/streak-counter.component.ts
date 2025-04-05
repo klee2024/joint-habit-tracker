0e9634674d31833e4f5f3e98f287e9fb4ac4890f
klee2024 <kaya.m.lee7@gmail.com>
2025-04-05 18:49:27 -0400

    create joint habit components

---
+++ joint-habit-app/src/app/components/streak-counter/streak-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-streak-counter',
+  imports: [],
+  templateUrl: './streak-counter.component.html',
+  styleUrl: './streak-counter.component.css'
+})
+export class StreakCounterComponent {
+
+}
