07938dbe1f01b831d52f17b3b335751de7ec09a8
klee2024 <kaya.m.lee7@gmail.com>
2025-04-19 19:45:30 -0400

    refactor child components of Habit main to get data from signal

--- joint-habit-app/src/app/components/habit-past-days/habit-past-days.component.ts
+++ joint-habit-app/src/app/components/habit-past-days/habit-past-days.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { HabitsService } from '../../services/habits.service';
+import { Component, inject, OnInit, signal, input } from '@angular/core';
 import { HabitDay } from '../../model/habitDay.type';
-import { catchError } from 'rxjs';
-import { Habit } from '../../model/habit.type';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,25 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './habit-past-days.component.html',
   styleUrl: './habit-past-days.component.css',
 })
-export class HabitPastDaysComponent implements OnInit {
-  habitService = inject(HabitsService);
-  habitItems = signal<Array<HabitDay>>([]);
-  ngOnInit(): void {
-    // this returns an observable
-    this.habitService
-      .getHabit('67edcb728fac06d8f2f5a7f2')
-      .pipe(
-        catchError((err) => {
-          console.log(err);
-          throw err;
-        })
-      )
-      .subscribe((habit: Habit) => {
-        console.log(habit);
-        // TODO: figure out a better way for this
-        this.habitItems.set(
-          Array.isArray(habit.habitDays) ? habit.habitDays : [habit.habitDays]
-        );
-      });
-  }
+export class HabitPastDaysComponent {
+  pastDays = input<Array<HabitDay>>();
 }
