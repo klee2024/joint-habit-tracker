a5851f8e21549bdc705dbe7beaf5097faddbd17b
klee2024 <kaya.m.lee7@gmail.com>
2025-04-20 17:08:16 -0400

    create new login component

---
+++ joint-habit-app/src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  // get user input on submit
+  // call backend API to get user id
+  // set userId in local storage
+  // navigate to the habits screen
+  // const userId = user._id? || ""
+  // localStorage.setItem("id", userId);
+}
