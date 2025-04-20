4df3ee5f8a1f22eed2595e72e05aeb5eac8194c4
klee2024 <kaya.m.lee7@gmail.com>
2025-04-20 17:06:02 -0400

    implement routes for habit and login

--- joint-habit-app/src/app/app.routes.ts
+++ joint-habit-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HabitMainComponent } from './components/habit-main/habit-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'habit', component: HabitMainComponent },
+];
