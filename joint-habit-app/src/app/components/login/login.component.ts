3fc37c01747d9a6bd76ba97115d8e4215743cbc5
klee2024 <kaya.m.lee7@gmail.com>
2025-04-20 17:33:41 -0400

    created a basic login form

--- joint-habit-app/src/app/components/login/login.component.ts
+++ joint-habit-app/src/app/components/login/login.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
-  // get user input on submit
-  // call backend API to get user id
-  // set userId in local storage
-  // navigate to the habits screen
-  // const userId = user._id? || ""
-  // localStorage.setItem("id", userId);
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
 }
+
+// get user input on submit
+// call backend API to get user id
+// set userId in local storage
+// navigate to the habits screen
+// const userId = user._id? || ""
+// localStorage.setItem("id", userId);
