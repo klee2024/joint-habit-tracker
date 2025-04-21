0e8489503511fd8bbcd5ac5eb4aa6ce0a0912f35
klee2024 <kaya.m.lee7@gmail.com>
2025-04-21 19:09:07 -0400

    create new component for users' habit view

---
+++ joint-habit-app/src/app/components/users-habits/users-habits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-habits',
+  imports: [],
+  templateUrl: './users-habits.component.html',
+  styleUrl: './users-habits.component.css',
+})
+export class UsersHabitsComponent {
+  // get the user from local storage
+  // get all their habits
+}
