d8181f7b1a710d6082a391b2bc9b86d732d8e336
klee2024 <kaya.m.lee7@gmail.com>
2025-04-25 10:37:19 -0400

    implement simple navbar

--- joint-habit-app/src/app/components/navbar/navbar.component.ts
+++ joint-habit-app/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  ngOnInit() {}
+}
