3165545ad956b1db54326ad485b0696f2fd17d2f
klee2024 <kaya.m.lee7@gmail.com>
2025-04-25 11:16:46 -0400

    enable dynamic navigation to habit from user's habits through query params

--- joint-habit-app/src/app/components/habit-main/habit-main.component.ts
+++ joint-habit-app/src/app/components/habit-main/habit-main.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, OnDestroy } from '@angular/core';
 import { HabitTodayComponent } from '../habit-today/habit-today.component';
 import { HabitPastDaysComponent } from '../habit-past-days/habit-past-days.component';
 import { StreakCounterComponent } from '../streak-counter/streak-counter.component';
@@ -6,6 +6,8 @@ import { HabitsService } from '../../services/habits.service';
 import { CommonModule } from '@angular/common';
 import { Habit } from '../../model/habit.type';
 import { catchError } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-habit-main',
@@ -18,22 +20,40 @@ import { catchError } from 'rxjs';
   templateUrl: './habit-main.component.html',
   styleUrl: './habit-main.component.css',
 })
-export class HabitMainComponent {
+export class HabitMainComponent implements OnInit, OnDestroy {
+  // TODO: look into a better way to ensure values are initialized properly and not null
+
   habitService = inject(HabitsService);
-  // TODO: look into a better way for this
   habit = signal<Habit>(null as unknown as Habit);
+  habitId: string | null = null;
+  private queryParamsSubscription: Subscription | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
   ngOnInit(): void {
-    this.habitService
-      .getHabit('67edcb728fac06d8f2f5a7f2')
-      .pipe(
-        catchError((err) => {
-          console.log('this is the error: ', err);
-          throw err;
-        })
-      )
-      .subscribe((habit: Habit) => {
-        console.log('main habit: ', habit);
-        this.habit.set(habit);
-      });
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params) => {
+        this.habitId = params['id'];
+      }
+    );
+    if (this.habitId) {
+      this.habitService
+        .getHabit(this.habitId)
+        .pipe(
+          catchError((err) => {
+            console.log('this is the error: ', err);
+            throw err;
+          })
+        )
+        .subscribe((habit: Habit) => {
+          console.log('main habit: ', habit);
+          this.habit.set(habit);
+        });
+    }
+  }
+  ngOnDestroy() {
+    if (this.queryParamsSubscription) {
+      this.queryParamsSubscription.unsubscribe();
+    }
   }
 }
